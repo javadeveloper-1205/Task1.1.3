6a3d28d3d6c885aae559c7940195ce9519667729
Greg <grygoriy23@gmail.com>
2020-10-24 09:15:58 -0500

    Task_1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,34 +2,88 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try {
+            connection.setAutoCommit(false);
+            connection.createStatement().executeUpdate("CREATE TABLE main.table_User IF NOT EXISTS" +
+                    "(INTEGER id, name text, lastName text, INTEGER age)");
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            connection.createStatement().executeUpdate("DROP TABLE main.table_User");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            connection.createStatement().executeUpdate("INSERT INTO main.table_User (name, lastName, age)" +
+                    "VALUES ('" + name + "', " + "'" + lastName + "', " + age + ")");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            connection.createStatement().executeUpdate("DELETE FROM main.table_User WHERE id = " + id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List<User> list = new LinkedList<>();
+        Statement statement = null;
+        String sql = "SELECT * FROM main.table_User";
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("Name"));
+                user.setLastName(resultSet.getString("LastName"));
+                user.setAge(resultSet.getByte("age"));
+                list.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        try {
+            connection.createStatement().executeUpdate("TRUNCATE main.table_user IGNORE DELETE TRIGGERS DROP STORAGE");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
+
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:/Users/grygoriyzhurylo/IdeaProjects/JM_PredProject/Task_1.1.3/test.db");
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Something get wrong" + e.getMessage());
+        }
+        return connection;
+    }
 }
